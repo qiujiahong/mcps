c613dacfbe7ab4b8bb16c72e19fad211609dfbd6
邱家洪 <qiujiahong@crdigital.com.cn>
2025-07-12 23:55:43 +0800

    完成header的请求

--- 03-passing-runtime-headers/weather_server.py
+++ 03-passing-runtime-headers/weather_server.py
@@ -1,12 +1,22 @@
 # weather_server.py
-from typing import List
-from mcp.server.fastmcp import FastMCP
+# from typing import List
+# from mcp.server.fastmcp import FastMCP
+from fastmcp import FastMCP
+from fastmcp.server.dependencies import get_http_request
+from starlette.requests import Request
 
 mcp = FastMCP("Weather")
 
 @mcp.tool()
 async def get_weather(location: str) -> str:
     """Get weather for location."""
+    request: Request = get_http_request()
+    user_agent = request.headers.get("user-agent", "Unknown")
+    Authorization = request.headers.get("Authorization", "Unknown")
+    XCustomHeader = request.headers.get("X-Custom-Header", "Unknown")
+    print(f"user_agent = {user_agent}")
+    print(f"Authorization = {Authorization}")
+    print(f"XCustomHeader = {XCustomHeader}")
     return "It's always sunny in New York"
 
 if __name__ == "__main__":
